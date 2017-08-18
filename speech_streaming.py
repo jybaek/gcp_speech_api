fe884190324db3b69408eeaa5e70d4d022cb0c26
백재연 <jybaek@baegjaeyeon-ui-MacBook-Pro.local>
2017-08-18 13:54:53 +0900

    Add streaming recognition

---
+++ speech_streaming.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import io
+import os
+
+# Imports the Google Cloud client library
+try:
+    from google.cloud import speech
+except ImportError:
+    print("Error speech import error")
+    exit(255)
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+# Instantiates a client
+client = speech.SpeechClient()
+config = types.RecognitionConfig(
+    encoding='LINEAR16',
+    language_code='ko-KR',
+    sample_rate_hertz=16000)
+
+# Loads the audio into memory
+with io.open("./test.raw", 'rb') as stream:
+    requests = [types.StreamingRecognizeRequest(
+        audio_content=stream.read(),
+    )]
+
+config=types.StreamingRecognitionConfig(config=config)
+responses = speech.SpeechClient().streaming_recognize(config,requests)
+
+for response in responses:
+    for result in response.results:
+        for alternative in result.alternatives:
+            print('=' * 20)
+            print('transcript: {}'.format(alternative.transcript))
+            print('confidence: {}'.format(alternative.confidence))
